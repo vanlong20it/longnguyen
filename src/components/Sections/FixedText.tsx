17a8f75d069ec0ff1c33df60b7c8c9e7343c3130
longnguyen <vanlong20it@gmail.com>
2023-11-06 01:23:56 +0700

    Optimize image

--- src/components/Sections/FixedText.tsx
+++ src/components/Sections/FixedText.tsx
@@ -8,7 +8,7 @@ interface Props {
 const FixedText: FC<Props> = ({ title, backgroundTitle }) => {
   return (
     <div className="relative text-center">
-      <div className="relative text-9xl text-gray-300/10">
+      <div className="relative text-9xl text-gray-300/10 select-none">
         {backgroundTitle}
       </div>
       <div className="absolute left-1/2 text-5xl top-1/2 text-white -translate-x-1/2 -translate-y-1/2">
